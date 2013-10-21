dc1674f6a430c3c7e9a47e4c767e1ec526674ad7
cmichi <mich@elmueller.net>
2013-10-21 17:13:09 +0200

    refactoring + rewriting code

--- crawler.js
+++ crawler.js
@@ -1,53 +1,51 @@
-/**
- * Simple Crawler for the uulm-addressbook <http://ab.uni-ulm.de/>.
- * We use the data as demodata for our software project.
- *
- * Written by <michael-4.mueller@uni-ulm.de>, 02.2011
- */
-
 var request = require('ahr');
 var jsdom = require("jsdom");
+var fs = require("fs");
 
+/* create alphabet */
 var alphabet = ['Ü', 'Ä', 'Ö'];
-
-
 for (var i = 65; i < 65 + 26; i++) {
 	alphabet.push(String.fromCharCode(i));
 }
 
-var Client = require('mysql').Client,
-client = new Client();
-//client.debug = true;
+alphabet = ['A'];
 
-client.user = 'root';
-client.password = 'root';
-client.database = 'scrape';
-client.port = '/Applications/MAMP/tmp/mysql/mysql.sock';
-client.host = 'localhost';
-///console.log(client);
+/* query each thing in alphabet. if return value == "too many results" pop
+this element and insert a new one containing the query + each alphabet
+char. */
+var not_yet_returned = 0;
 
-
-client.connect();
-
-//console.log(client.ping());
-//client.query("INSERT INTO tasks SET term = ?, done = ?", [alphabet[0], 1]);
-
-function eliminate() {
-	var eliminateCombos = ['ZZ', 'XX', 'YY', 'PP', 'WW', 'UU', 'GG', 'KK', 'LL', 'PP', 'YQ', 'QY', 'BB', 'CC', 'DD',
-	    'EE', 'FF', 'GG', 'HH', 'KK', 'MM', 'ÄÄ', 'ÜÜ', 'ÖÖ', 'NN', 'QQ', 'RR', 'SS', 'TT', 'VV', 'YX', 'XY', 'YZ',
-	    'ÄÜ', 'ÄÖ', 'ÖÄ', 'ÜÄ', 'ÄÜ', 'XÜ', 'ÄZ', 'WX'];
-
-	for (var i = 0; i < searchDomains.length; i++) {
-		for (var p = 0; p < eliminateCombos.length; p++) {
-			if (searchDomains[i] == eliminateCombos[p]) 
-				searchDomains.pop(i);
-		}
+function worker(q) {
+	if (not_yet_returned > 10) {
+		console.log("timeout")
+		setTimeout(500, worker(q));
+	} else if (not_yet_returned <= 10) {
+		handle(q);
 	}
+};
+
+function handle(q) {
+	console.log("handling " + q)
+	not_yet_returned++;
+	query_name(q, function(result) {
+		console.log(result);
+		not_yet_returned--;
+		if (result === false) {
+			/* populate with "foo" + [a, b, c, ...] */
+			for (var a in alphabet) {
+				worker(q + alphabet[a]);
+			}
+		} else {
+			fs.appendFile('results.csv', result, function (err) {
+				if (err) throw err;
+			});
+		}
+	});
 }
-//console.log(searchDomains);
 
-
-//console.log("'Name';'Einrichtung';'Gebaeude';'E-Mail';'Telefon';'Telefax';'Mobil';'WWW';");
+for (var a in alphabet) {
+	worker(alphabet[a]);
+}
 
 String.prototype.trim = function() { return this.replace(/^\s+|\s+$/, ''); };
 
@@ -93,20 +91,16 @@ function getTdContent(content, nr, typ) {
 }
 
 
-function callit(term) {
-	request.get("http://ab.uni-ulm.de/ab/search.pl", {group: "all", lang: "de", query: term}, {timeout: 10000})
-	//request.get("http://localhost:8888/scrape.html", {group: "all", lang: "de", query: term}, {timeout: 10000})
+function query_name(term, cb) {
+	request.get("http://ab.uni-ulm.de/ab/search.pl", 
+			{group: "all", lang: "de", query: term}, {timeout: 10000})
 	.when(function (err, Xhr_Or_NodeResponse, data) {
 		content = data.toString("utf8");
+		//console.log(content);
+		var allData = [];
 
 		if (content.match(/Bitte genaueren Suchbegriff eingeben./gi)) {
-			client.query("UPDATE tasks SET done = 1 WHERE term = '" + term + "'");
-			//console.log("<== " + term);
-			for (var j = 0; j < alphabet.length; j++) {
-				client.query("INSERT INTO tasks SET term = ?, done = ?", [term + alphabet[j], 0]);
-				//console.log("==> " + term + alphabet[j]);
-			}
-			console.log('--> published new tasks for ' + term);
+			cb(false);
 		} else {
 			content = content.substr(content.indexOf("<h2>Ihr Suchergebnis:</h2><br>") + 30);
 			content = content.substr(0, content.indexOf("<hr />"));
@@ -125,24 +119,15 @@ function callit(term) {
 					currentData.mobil = getTdContent(currentOne, td++);
 					currentData.www = getTdContent(currentOne, td++);
 
-					//currentData.www = currentData.www.replace("'", '"');
-					//console.log(currentData.www);
-//client.debug = true;
-					client.query('INSERT INTO scrapes ' + 
-						     'SET `name` = ?, einrichtung = ?, gebaeude = ?,' +
-					     	     'email = ?, telefon = ?, telefax = ?, mobil = ?, www = ?',
-						[currentData.name,  currentData.einrichtung, currentData.gebaeude,
-						currentData.email, currentData.telefon, currentData.telefax, currentData.mobil,
-						currentData.www]);
-
-					//allData.push(currentData);
 					content = content.substr(content.indexOf("</table>") + 8, content.length);
 					pos = content.indexOf("<h3>");
+					allData.push(currentData);
 				}
 
-				client.query("UPDATE tasks SET done = 1 WHERE term = '" + term + "'");
 				console.log("done with " + term);
+				console.log(allData);
 				//outputAsCsv(allData, false);
+				cb(allData);
 		}
 
 
@@ -161,22 +146,4 @@ function outputAsCsv(json, withHeader) {
 	}
 }
 
-// max 10 tasks
-client.query("SELECT * FROM tasks WHERE done = 0 LIMIT 100",
-	function selectCb(err, results, fields) {
-			if (err) {
-			throw err;
-		}
-
-		//console.log(results);
-		//console.log(fields);
-		//client.end();
-		for (var i in results) {
-			console.log("going for " + results[i].term);
-			callit(results[i].term);
-		}
-	}
-);
-
-//callit("ME");
 
